44947d181cc9e7b80fd6c1d85972043462f12005
AlexPhamNgoQuoc <131502621+AlexPhamNgoQuoc@users.noreply.github.com>
2023-04-22 09:46:31 +0700

    install.py now doned
    Doned install

--- install.py
+++ install.py
@@ -1,3 +1,4 @@
+print("Loading Complete!")
 try:
     import hashlib, os, requests
     #from datetime import datetime
@@ -25,8 +26,9 @@ while True:
         print("Install Complete!")
         os.system("ecodefenderfull.exe")
     elif hashed_key in [hashlib.sha256(k.encode()).hexdigest() for k in valid_ekeys]:
+        print('Start installing...')
         s = requests.get('https://download1475.mediafire.com/0xmkap9ui7ngLpbLvAAKM4wlUL7cbs81-6_R2Rjqs7QoZcd_wxzX9eVFLcq0GCiERtpBbs8b6px4yJ2VAU70UtdSqw/eh6z4kspixt011g/ecodefendereditable.gmz', allow_redirects=True)
-        open('ecodefendereditable.gmz', 'wb').write(r.content)
+        open('ecodefendereditable.gmz', 'wb').write(s.content)
         print("Successfully download!")
         #os.system("ecodefenderfull.exe")
         #os.system("attrib -r -h -s ecodefendereditable.gmz")
